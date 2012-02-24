aaf16fc5f99d4714a22b8cca61f908b81b0b5f41
valeron <t-gult_23@hotmail.com>
2012-02-24 13:49:37 +0200

    finish layout & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Borcumborc::Application.routes.draw do
 
+  get "users/new"
+
+  match '/signup',  to: 'users#new'
+
   match '/about',   to: 'static_pages#about'
 
   match '/login',    to: 'static_pages#login'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,26 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('Home')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About'" do
-      visit about_path
-      page.should have_selector('h1', text: 'About')
-    end
-
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | About")
-    end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "Login page" do
+    before { visit login_path }
 
-    it "should have the h1 'Login'" do
-      visit login_path
-      page.should have_selector('h1', text: 'Login')
-    end
-
-    it "should have the title 'Login'" do
-      visit login_path
-      page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App | Login")
-    end
+    it { should have_selector('h1',    text: 'Login') }
+    it { should have_selector('title', text: full_title('Login')) }
   end
 end
